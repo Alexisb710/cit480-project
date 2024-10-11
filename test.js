ab4a1f685bb2527d8f854f9716c50105670f0f53
Alexisb710 <alexisbm710@gmail.com>
2024-10-10 17:48:50 -0700

    Add test file with linting issues

---
+++ test.js
@@ -0,0 +1,12 @@
+// test.js
+
+// Intentionally leaving a semi-colon out, declaring a variable that is never used,
+// and using inconsistent quotes.
+var unusedVar = "This is a test";
+
+function sayHello() {
+  console.log("Hello, World!");
+  console.log("Inconsistent quotes");
+}
+
+sayHello();
